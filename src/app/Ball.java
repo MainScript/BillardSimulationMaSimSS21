8708b3e96729f3ca2ca1282d7c18c810519081a4
Louciandra <37979646+MainScript@users.noreply.github.com>
2021-05-22 14:53:52 +0200

    update Ball

--- src/app/Ball.java
+++ src/app/Ball.java
@@ -1,19 +1,23 @@
 package app;
 
 import java.awt.Graphics;
+import java.util.Arrays;
 
 
 public class Ball {
 	double[] position = new double[2];
-	double[] velocity = new double[2];
+	double[] v = new double[2];
+	double[] u = new double[2];
+	double[][] u_comp = new double[2][2];
 	int d;
 	int minV;
 	double mass;
+	
 	public Ball(double startX, double startY, double _vX, double _vY, int _diameter, double _mass) {
 		this.position[0] = startX;
 		this.position[1] = startY;
-		this.velocity[0] = _vX;
-		this.velocity[1] = _vY;
+		this.v[0] = _vX;
+		this.v[1] = _vY;
 		this.mass = _mass;
 		this.d = _diameter;
 	}
@@ -28,35 +32,35 @@ public class Ball {
 		this.checkForBox(width, height, damp);
 		this.checkForBall(baelle, damp);
 		for (int i = 0; i < 2; i++) {
-			this.position[i] += this.velocity[i] / _0_Constants.FPS;
+			this.position[i] += this.v[i] / _0_Constants.FPS;
 		}
 	}
 	
 	public void checkForBox(int width, int height, double damp) {
 		double[] futurePos = new double[2];
 		for (int i = 0; i < 2; i++) {
-			futurePos[i] = this.position[i] + this.velocity[i] / _0_Constants.FPS;
+			futurePos[i] = this.position[i] + this.v[i] / _0_Constants.FPS;
 		}
 		
 		if (futurePos[0] < 0 || futurePos[0]+this.d > width) {
-			this.velocity[0] *= -damp;
+			this.v[0] *= -damp;
 		}
 		if (futurePos[1] < 0) {
-			this.velocity[1] *= -damp;
+			this.v[1] *= -damp;
 		}
 		if (futurePos[1]+this.d > height) {
-			this.velocity[1] *= -damp;
+			this.v[1] *= -damp;
 		}
 		
 		if (futurePos[1] < 0 || futurePos[1]+this.d > height) {
-			if (Math.abs(this.velocity[1]) < this.minV) {
-				this.velocity[1] *= 0;
+			if (Math.abs(this.v[1]) < this.minV) {
+				this.v[1] *= 0;
 			}
 		}
 		
 		if (futurePos[0] < 0 || futurePos[0]+this.d > width) {
-			if (Math.abs(this.velocity[0]) < this.minV) {
-				this.velocity[0] *= 0;
+			if (Math.abs(this.v[0]) < this.minV) {
+				this.v[0] *= 0;
 			}
 		}
 	}
@@ -67,13 +71,15 @@ public class Ball {
 				continue;
 			} else {
 				if (distance(this.position[0]+this.d/2, this.position[1]+this.d/2, ball.position[0]+ball.d/2, ball.position[1]+ball.d/2) < this.d/2+ball.d/2) {
-					double num = this.velocity[0]*(this.mass - ball.mass) + 2 * ball.mass * ball.velocity[0];
+					this.splitU(baelle);
+					
+					double num = this.v[0]*(this.mass - ball.mass) + 2 * ball.mass * ball.v[0];
 					double den = this.mass + ball.mass;
 					double newvel = num/den;
 					
-					num = ball.velocity[0] * (ball.mass - this.mass) + 2 * this.mass * this.velocity[0];
-					ball.velocity[0] = (num/den)*damp;
-					this.velocity[0] = newvel*damp;
+					num = ball.v[0] * (ball.mass - this.mass) + 2 * this.mass * this.v[0];
+					ball.v[0] = (num/den)*damp;
+					this.v[0] = newvel*damp;
 				}
 			}
 		}
@@ -85,6 +91,40 @@ public class Ball {
 		double num2 = Math.pow(y2 - y1, 2);
 		dist = Math.sqrt(num1 + num2);
 		return dist;
+	}
+	
+	public void calcCM(Ball[] baelle) {
+		for(Ball ball :  baelle) {
+			if(this == ball) {
+				continue;
+			} else {
+				for(int i = 0; i < 2; i++) {
+					this.u[i] = (ball.mass/(this.mass + ball.mass)) * (this.v[i] - ball.v[i]);
+				}
+			}
+		}
+	}
+	
+	public void splitU(Ball[] baelle) {
+		this.calcCM(baelle);
+		for(Ball ball :  baelle) {
+			if(this == ball) {
+				continue;
+			} else {
+				double[] b = {ball.position[0] - this.position[0], ball.position[1] - this.position[1]};
+				System.out.println(b[1]);
+				for(int i = 0; i < 2; i++) {
+					this.u_comp[0][i] = (this.u[i] - b[i])/(b[i] * b[i]) * b[i];
+				}
+				for(int i = 0; i < 2; i++) {
+					this.u_comp[1][i] = this.u[i] - this.u_comp[0][i];
+				}
+			}
+		}
+		
+		System.out.println(Arrays.toString(this.u));
+		double[] test = {this.u_comp[0][0] + this.u_comp[1][0], this.u_comp[0][1] + this.u_comp[1][1]};
+		System.out.println(Arrays.toString(test));
 	}
 }
 
